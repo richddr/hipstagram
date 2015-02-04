a7f6963fa010ca3087163b6c43a126aa47a97b05
Richard Garcia <richard.g@outlook.com>
2015-02-04 12:31:27 -0400

    add comment model

--- app/models/hashtag.rb
+++ app/models/hashtag.rb
@@ -1,2 +1,6 @@
 class Hashtag < ActiveRecord::Base
+	validates :name, presence: true, uniqueness: true
+	# has_and_belongs_to_many :microposts, class_name: "Micropost",
+	# 																		 join_table: :hashtags_microposts,
+	# 																		 foreign_key: "hashtag_id"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150204061159) do
+ActiveRecord::Schema.define(version: 20150204162841) do
+
+  create_table "comments", force: :cascade do |t|
+    t.text     "content"
+    t.integer  "user_id"
+    t.integer  "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "comments", ["post_id"], name: "index_comments_on_post_id"
+  add_index "comments", ["user_id", "post_id", "created_at"], name: "index_comments_on_user_id_and_post_id_and_created_at", unique: true
+  add_index "comments", ["user_id"], name: "index_comments_on_user_id"
 
   create_table "hashtags", force: :cascade do |t|
     t.string   "name"
