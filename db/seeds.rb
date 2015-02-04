4ea2582338bb48cfba92049c8a96a6ca0572df45
Richard Garcia <richard.g@outlook.com>
2015-02-04 01:57:24 -0400

    adding guest account to db seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create!(name: "Example User",
+						email: "example@hipstagram.org",
+						password: "foobar",
+						password_confirmation: "foobar",
+						admin: true,
+						activated: true,
+						activated_at: Time.zone.now,
+						username: "examuser")
+
+User.create!(name: "Guest User",
+						email: "guest@hipstagram.com",
+						password: "foobar",
+						password_confirmation: "foobar",
+						admin: false,
+						activated: true,
+						activated_at: Time.zone.now,
+						username: "guest")
